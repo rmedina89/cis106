422c6263a3b0e0543c56e7cedb68a3b63545e02a
rmedina89 <158505466+rmedina89@users.noreply.github.com>
2024-02-29 11:30:45 -0500

    Add files via upload

---
+++ lab3/lab3 challenge.sh
@@ -0,0 +1,11 @@
+#Author: richard medina
+#Date: 2-17-24
+#Description: this is my attempt at the L3 challenge
+
+#!/bin/bash 
+echo -n "This script will print the current date: "
+date -r
+echo "This is some info about my system: "
+echo $HOSTNAME
+echo "This is the current user: "
+echo $USER
